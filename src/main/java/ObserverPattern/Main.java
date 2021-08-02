4bd5102cae48c69eaec2a234921e4582d987df42
Durjoy <da-durjoy@outlook.com>
2021-08-02 16:46:32 +0600

    Observer Pattern Set 2 Created

--- src/main/java/ObserverPattern/Main.java
+++ src/main/java/ObserverPattern/Main.java
@@ -1,15 +1,30 @@
 package ObserverPattern;
 
-class CricketData{
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+interface Subject
+{
+    void registerObserver(Observer or);
+    void unregisterObserver(Observer or);
+    void notifyObserver();
+}
+
+interface Observer
+{
+    void update(int runs,int wickets,float overs);
+}
+
+class CricketData implements Subject{
     int runs,wickets;
     float overs;
-    CurrentScoreDisplay CSD;
-    AverageScoreDisplay ASD;
-
-    public CricketData(CurrentScoreDisplay CSD, AverageScoreDisplay ASD) {
-        this.CSD = CSD;
-        this.ASD = ASD;
+    ArrayList<Observer> ObserverList;
+    public CricketData()
+    {
+        ObserverList=new ArrayList<>();
     }
+
     private int getLatestRuns() {
 
         //50 for simplicity
@@ -21,31 +36,45 @@ class CricketData{
     }
     private float getLatestOvers()
     {
-        return 6.7f;
+        return 7.3f;
     }
     public void getUpdated()
     {
         runs=getLatestRuns();
         wickets=getLatestWickets();
         overs=getLatestOvers();
-
-        ASD.update(runs,wickets,overs);
-        CSD.Update(runs,wickets,overs);
+        notifyObserver();
     }
-
+    @Override
+    public void registerObserver(Observer or) {
+        ObserverList.add(or);
+    }
+    @Override
+    public void unregisterObserver(Observer or) {
+        ObserverList.remove(ObserverList.indexOf(or));
+    }
+    @Override
+    public void notifyObserver() {
+        for (Iterator<Observer> iterator=ObserverList.iterator();
+        iterator.hasNext();)
+        {
+            Observer o=iterator.next();
+            o.update(runs,wickets,overs);
+        }
+    }
 }
 
-class AverageScoreDisplay
+class AverageScoreDisplay implements Observer
 {
     private float runrate;
     private int predicateScore;
+    @Override
     public void update(int runs, int wicket, float overs)
     {
         this.runrate=(float)runs/overs;
         this.predicateScore=(int)(this.runrate*50);
         display();
     }
-
     private void display() {
         System.out.println("\nAverage Score Display:\n" +
                 "Run Rate: " + runrate +
@@ -54,29 +83,31 @@ class AverageScoreDisplay
 }
 
 
-class CurrentScoreDisplay
+class CurrentScoreDisplay implements Observer
 {
     private int runs,wicket;
     private float overs;
-
-    public void Update(int runs, int wicket, float overs) {
-        this.runs = runs;
-        this.wicket = wicket;
-        this.overs = overs;
-        display();
-    }
-
     private void display() {
         System.out.println("\nCurrent Score Display: \n" +
                 "Runs: " + runs +"\nWickets:"
                 + wicket + "\nOvers: " + overs );
+    }
+    @Override
+    public void update(int runs, int wickets, float overs) {
+        this.runs = runs;
+       this.wicket = wickets;
+        this.overs = overs;
+        display();
     }
 }
 public class Main {
     public static void main(String[] args) {
         var ASD=new AverageScoreDisplay();
         var CSD=new CurrentScoreDisplay();
-        var Cdata=new CricketData(CSD,ASD);
+        var Cdata=new CricketData();
+        Cdata.registerObserver(ASD);
+        Cdata.registerObserver(CSD);
         Cdata.getUpdated();
+        //Cdata.notifyObserver();
     }
 }
