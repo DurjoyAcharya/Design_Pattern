380eedf3447f5b9231367a848c46a98daf919adf
Durjoy <da-durjoy@outlook.com>
2021-12-21 02:06:50 +0600

    Memento Design Pattern

---
+++ src/main/java/MementoDesignPattern/Element.java
@@ -0,0 +1,14 @@
+package MementoDesignPattern;
+
+public class Element {
+   protected String element;
+    public Element(String element) {
+        this.element = element;
+    }
+    @Override
+    public String toString() {
+        return "Element{" +
+                "element='" + element + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/MementoDesignPattern/Palette.java
@@ -0,0 +1,35 @@
+package MementoDesignPattern;
+
+import java.util.ArrayList;
+
+public class Palette {
+    ArrayList<Element>elements=new ArrayList<>();
+    public void addElement(Element element)
+    {
+        elements.add(element);
+    }
+    public ArrayList<Element> getElements()
+    {
+        return (ArrayList<Element>) elements.clone();
+    }
+    public GUIMemento save()
+    {
+        return new GUIMemento(getElements());
+    }
+    public void revert(GUIMemento guiMemento)
+    {
+        elements=guiMemento.getElements();
+    }
+    static class GUIMemento
+    {
+        ArrayList<Element> elements;
+
+        public GUIMemento(ArrayList<Element> elements) {
+            this.elements = elements;
+        }
+        private ArrayList<Element> getElements()
+        {
+            return elements;
+        }
+    }
+}
