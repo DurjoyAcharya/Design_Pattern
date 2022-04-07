db80c0204ac602ede629f523f4e360ca5f60e873
Durjoy <da-durjoy@outlook.com>
2022-04-07 13:41:37 +0600

    GetUpdated

--- src/main/java/ProxyPattern/ThirdPartyYoutubeClass.java
+++ src/main/java/ProxyPattern/ThirdPartyYoutubeClass.java
@@ -5,7 +5,27 @@ import java.util.HashMap;
 public class ThirdPartyYoutubeClass implements ThirdPartyYoutubeLib {
     @Override
     public HashMap<String, Videos> popularVideos() {
+        ConnectingToServer("");
+        return getRandomVideos();
+    }
+
+    private HashMap<String, Videos> getRandomVideos() {
+
         return null;
+    }
+    private void ConnectingToServer(String s) {
+        System.out.println("Connecting to Server....");
+        ExperienceNetworkLatency();
+        System.out.println("Connected!");
+    }
+
+    private void ExperienceNetworkLatency() {
+
+    }
+
+    private int Random(int min,int max)
+    {
+        return min+(int)((Math.random())*((max-min)+1));
     }
     @Override
     public Videos getVideos(String videoId) {
